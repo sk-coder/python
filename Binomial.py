ac9e0ecf1a9ca6f53dc1c5acccace1bf2c9d5e07
Scott Karneth <sk954@outlook.com>
2020-03-03 16:24:44 -0500

    Added initial (On) solution. Still misses on at least one case. Needs improvement.

--- Binomial.py
+++ Binomial.py
@@ -1,7 +1,7 @@
 import math
 
 
-def Choose(n, k):
+def binomial_coefficient(n, k):
     try:
         n = int(n)
     except ValueError:
@@ -13,36 +13,33 @@ def Choose(n, k):
         return "invalid"
 
 
-    if n >= k and k >= 0:
+    if n >= k >= 0:
         return math.factorial(n) // math.factorial(k) // math.factorial(n - k)
 
     return "invalid"
 
-output = Choose(1, -3)
 
-print output
+if __name__ == '__main__':
+    # test 1 - valid case
+    output = binomial_coefficient(4, 2)
+    print "Should be Valid:" + str(output)
+    if output == 6:
+        print "Test Worked"
+    else:
+        print "You messed this up"
 
+    # test 2 - valid case
+    output = binomial_coefficient(8, 4)
+    print "Should be Valid:" + str(output)
 
-# test 1 - valid case
-output = Choose(4,2)
-print "Should be Valid:" + str(output)
-if output == 6:
-    print "Test Worked"
-else:
-    print "You messed this up"
+    # test 3 - invalid case
+    output = binomial_coefficient(2, 4)
+    print "Should be Invalid:" + str(output)
 
-# test 2 - valid case
-output = Choose(8,4)
-print "Should be Valid:" + str(output)
+    # test 4 - invalid case
+    output = binomial_coefficient(4, -2)
+    print "Should be Invalid:" + str(output)
 
-# test 3 - invalid case
-output = Choose(2, 4)
-print "Should be Invalid:" + str(output)
-
-# test 4 - invalid case
-output = Choose(4,-2)
-print "Should be Invalid:" + str(output)
-
-# test 5 string values
-output = Choose("foo", "bar")
-print "Should be Invalid:" + str(output)
+    # test 5 string values
+    output = binomial_coefficient("foo", "bar")
+    print "Should be Invalid:" + str(output)
