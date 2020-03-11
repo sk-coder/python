30d17ba5214c8a9d9b5a823fdf2f9455345147d4
Scott Karneth <sk954@outlook.com>
2020-03-11 09:32:45 -0400

    Added fully manual implementation of reversing a string

--- ReverseString.py
+++ ReverseString.py
@@ -2,7 +2,15 @@ my_string = "Hello World"
 
 output = ""
 
+# with built in reversed method
 for c in reversed(my_string):
     output = output + c
+
+print output
+
+# with manual traversal
+output = ""
+for i in range(len(my_string) - 1, -1, -1):
+    output += my_string[i]
 
 print output
